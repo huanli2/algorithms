bf4cb624ac3c9cfd4a41e3640cbe12442bca22ae
huanli2 <nier_123@yeah.net>
2014-11-04 18:31:42 +0800

    improve Binary_Tree_Postorder_Traversal.cpp's format

--- leetcode/Binary_Tree_Postorder_Traversal.cpp
+++ leetcode/Binary_Tree_Postorder_Traversal.cpp
@@ -31,53 +31,54 @@ vector<int> postorderTraversal(TreeNode *root) {
     if(root != NULL) s.push(root);
     while(!s.empty()){
         TreeNode* cur = s.top();
-		if(cur->left == NULL && cur->right == NULL){
+	if(cur->left == NULL && cur->right == NULL){
             res.push_back(cur->val);
             s.pop();
         }
         if(cur->right != NULL) {
-			s.push(cur->right);
-			cur->right = NULL;
-		}
-		if(cur->left != NULL) {
-			s.push(cur->left);
-			cur->left = NULL;
-		} 
+	    s.push(cur->right);
+	    cur->right = NULL;
+	}
+	if(cur->left != NULL) {
+	    s.push(cur->left);
+	    cur->left = NULL;
+	} 
     }
     return res;
 }
 
 void print(vector<int> &v) {
-	for(vector<int>::iterator t = v.begin(); t != v.end(); ++t) {
-		cout << *t << " ";
-	}
-	cout << endl;
+    for(vector<int>::iterator t = v.begin(); t != v.end(); ++t) {
+	cout << *t << " ";
+    }
+    cout << endl;
 }
 
 void printByPostorder(TreeNode* root) {
-	if(root) {
-		printByPostorder(root->left);
-		printByPostorder(root->right);
-		cout << root->val << " ";
-	}
+    if(root) {
+	printByPostorder(root->left);
+	printByPostorder(root->right);
+	cout << root->val << " ";
+    }
 }
+
 int main()
 {
-	TreeNode t0(1);
-	TreeNode t1(2);
-	TreeNode t2(3);
-	TreeNode t3(4);
-	TreeNode t4(5);
-	TreeNode t5(7);
-	t0.left = &t1;
-	t0.right = &t2;
-	t1.left = &t3;
-	t1.right = &t4;
-	t2.right = &t5;
-	cout << "input tree:  " << endl;
-	printByPostorder(&t0);
-	cout << endl;
-	print(postorderTraversal(&t0));
-	getchar();
-	return 0;
+    TreeNode t0(1);
+    TreeNode t1(2);
+    TreeNode t2(3);
+    TreeNode t3(4);
+    TreeNode t4(5);
+    TreeNode t5(7);
+    t0.left = &t1;
+    t0.right = &t2;
+    t1.left = &t3;
+    t1.right = &t4;
+    t2.right = &t5;
+    cout << "input tree:  " << endl;
+    printByPostorder(&t0);
+    cout << endl << "post_order_traversal
+    print(postorderTraversal(&t0));
+    getchar();
+    return 0;
 }
